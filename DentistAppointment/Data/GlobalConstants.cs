4f94cb4d5b7d60ff1a9e3cfb9fc6b8b3603f7d8a
Viktoria Penkova <vicky.penkova@gmail.com>
2018-11-07 22:02:28 +0200

    Adding a folder for all the global variables in the project

--- DentistAppointment/Data/GlobalConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DentistAppointment.Data
-{
-    public class GlobalConstants
-    {
-        public const string AdminRole = "Admin";
-        public const string UserRole = "User";
-        public const string DentistRole = "Dentist";
-    }
-}
